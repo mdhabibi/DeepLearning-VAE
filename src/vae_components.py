5b62d35d5823aa8a8a3da7a9db0828f21db7e46d
mdhabibi <habibi.physics@gmai.com>
2023-12-15 21:18:52 +0100

    update vae_component.py

--- src/vae_components.py
+++ src/vae_components.py
@@ -11,20 +11,18 @@ class VAELossLayer(layers.Layer):
     of the Variational AutoEncoder (VAE)
     """
 
+    def __init__(self, beta=0.0005, **kwargs):
+        super().__init__(**kwargs)
+        self.beta = beta
+        
     def calculate_loss(self, original_input, reconstructed_output, mu, sigma):
         """
         Calculates VAE loss, which is the sum of the reconstruction loss and KL-divergence loss
         """
-        original_input  = K.flatten(original_input)
+        original_input = K.flatten(original_input)
         reconstructed_output = K.flatten(reconstructed_output)
-
-        # Reconstruction loss - binary crossentropy is used as the final layer uses sigmoid activation
         reconstruction_loss = keras.metrics.binary_crossentropy(original_input, reconstructed_output)
-
-        # KL divergence loss - acts as a regularizer- beta is changable
-        #kl_loss = -0.5 * K.mean(1 + sigma - K.square(mu) - K.exp(sigma), axis=-1)
-        #kl_loss = beta * K.mean(-0.5 * (1 + sigma - K.square(mu) - K.exp(sigma)), axis=-1)
-        kl_loss = -5e-4 * K.mean(1 + sigma - K.square(mu) - K.exp(sigma), axis=-1)
+        kl_loss = -self.beta * K.mean(1 + sigma - K.square(mu) - K.exp(sigma), axis=-1)
         
         self.add_metric(reconstruction_loss, name='reconstruction_loss')
         self.add_metric(kl_loss, name='kl_loss')
@@ -35,13 +33,7 @@ class VAELossLayer(layers.Layer):
         """
         Computes the loss and adds it to the layer's losses
         """
-        original_input = inputs[0]
-        reconstructed_output = inputs[1]
-        mu = inputs[2]
-        sigma = inputs[3]
-
+        original_input, reconstructed_output, mu, sigma = inputs
         loss = self.calculate_loss(original_input, reconstructed_output, mu, sigma)
-        self.add_loss(loss, inputs = inputs)
-
-        # Returns the original layer inputs
+        self.add_loss(loss, inputs=inputs)
         return original_input
